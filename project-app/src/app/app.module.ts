68ebe48b3fd3de20035beb30bd6e865f1ef8c12a
gabriela-stoineva-91 <114081727+gabriela-stoineva-91@users.noreply.github.com>
2023-07-10 19:24:09 +0300

    create header, footer, home

--- project-app/src/app/app.module.ts
+++ project-app/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { CoreComponent } from './core/core.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    CoreComponent
   ],
   imports: [
     BrowserModule,
