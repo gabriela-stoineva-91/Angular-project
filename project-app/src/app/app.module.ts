9a2a68490e5164e5a0f8923d18bcb62efca956ed
gabriela-stoineva-91 <114081727+gabriela-stoineva-91@users.noreply.github.com>
2023-07-17 22:54:13 +0300

    Recipes and Cookbook routes

--- project-app/src/app/app.module.ts
+++ project-app/src/app/app.module.ts
@@ -7,18 +7,28 @@ import { CoreModule } from './core/core.module';
 import { HomeComponent } from './home/home.component';
 import { UserModule } from './user/user.module';
 import { UserRoutingModule } from './user/user-routing.module';
+import { RecipeModule } from './recipe/recipe.module';
+import { RecipeRoutingModule } from './recipe/recipe-routing.module';
+import { CookbookModule } from './cookbook/cookbook.module';
+import { CookbookRoutingModule } from './cookbook/cookbook-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+  
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     CoreModule, 
     UserModule,
-    UserRoutingModule
+    UserRoutingModule,
+    RecipeModule,
+    RecipeRoutingModule,
+    CookbookModule,
+    CookbookRoutingModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- project-app/src/app/recipe/recipe-routing.module.ts
+++ project-app/src/app/recipe/recipe-routing.module.ts
@@ -1,7 +1,23 @@
-import { NgModule } from '@angular/core';
+import { NgModule, createComponent } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateComponent } from './create/create.component';
+import { EditComponent } from './edit/edit.component';
+import { DetailsComponent } from './details/details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'create',
+    component: CreateComponent,
+  },
+  {
+    path: 'edit',
+    component: EditComponent
+  },
+  {
+    path: 'details',
+    component: DetailsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
