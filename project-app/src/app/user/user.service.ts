50b32ea6ffd1fecf6d193700547732ce981b03af
gabriela-stoineva-91 <114081727+gabriela-stoineva-91@users.noreply.github.com>
2023-07-26 18:05:11 +0300

    add user basic service

--- project-app/src/app/user/user.service.ts
+++ project-app/src/app/user/user.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Subscription, tap } from 'rxjs';
+import { User } from '../types/user';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
+  user: User | undefined;
+  USER_KEY = '[user]';
 
-  constructor() { }
+  get isSingIn(): boolean {
+    return !!this.user;
+  }
+
+  constructor() {
+    // try {
+    //   const lsUser = localStorage.getItem(this.USER_KEY) || '';
+    //   this.user = JSON.parse(lsUser);
+    // } catch (error) {
+    //   this.user = undefined;
+    // }
+  }
+
+  singIn(): void {
+    // this.user = {
+    // };
+
+    // localStorage.setItem(this.USER_KEY, JSON.stringify(this.user));
+  }
+
+  singOut(): void {
+    // this.user = undefined;
+    // localStorage.removeItem(this.USER_KEY);
+  }
 }
