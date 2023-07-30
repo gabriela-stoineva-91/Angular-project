12cb120c4d75af6d9f908b9fa335d6349c9d0e45
gabriela-stoineva-91 <114081727+gabriela-stoineva-91@users.noreply.github.com>
2023-07-30 23:53:22 +0300

    implement see details function

--- project-app/src/app/recipe/details/details.component.ts
+++ project-app/src/app/recipe/details/details.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from 'src/app/types/recipe';
+import { RecipeService } from '../recipe.service';
+import { UserService } from 'src/app/user/user.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.css']
 })
-export class DetailsComponent {
+export class DetailsComponent implements OnInit{
+  recipe: Recipe | undefined;
 
+  constructor(
+    private recipeService: RecipeService,
+    private userService: UserService,
+    private activatedRoute: ActivatedRoute
+    ) {}
+
+  ngOnInit(): void {
+    this.takeRecipe();
+  }
+  takeRecipe() {
+    const recipeId = this.activatedRoute.snapshot.params['name'];
+
+    
+    console.log(recipeId)
+  }
 }
