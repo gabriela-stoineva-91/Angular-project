3845795afe037355b35881255cdcd7261f6a7ac3
gabriela-stoineva-91 <114081727+gabriela-stoineva-91@users.noreply.github.com>
2023-08-07 22:57:49 +0300

    implement not-found

--- project-app/src/app/app-routing.module.ts
+++ project-app/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { EditComponent } from './recipe/edit/edit.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
   {
@@ -12,6 +13,10 @@ const routes: Routes = [
     path: 'welcome',
     component: HomeComponent
   },
+  {
+    path: '**',
+    component: NotFoundComponent
+  }
 ];
 
 @NgModule({
